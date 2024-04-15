1614fde4e530bbd6b7387823e65170fbe060b407
Kevin Mejia <kwmejia9@gmail.com>
2024-04-15 11:18:06 -0500

    Portafolios meta

--- js/app.js
+++ js/app.js
@@ -10,11 +10,13 @@ function loadInfo() {
         tbody.innerHTML += `
         <tr>
             <td>${coder.nombre}</td>
-            <td>
-            <a href="${coder.linkGit}">${coder.usuarioGit}</a>
+            <td class="d-flex gap-2 ">
+            <a href="${coder.linkGit}" target="_blank"> 
+                <img class="rounded-circle" src="img/5284.jpg" height="20"/>
+            ${coder.usuarioGit}</a>
             </td>
             <td>
-            <a href="${coder.linkPortafolio}">Ver</a>
+            <a href="${coder.linkPortafolio}" target="_blank">Ver</a>
             </td>
         </tr>
         `;
